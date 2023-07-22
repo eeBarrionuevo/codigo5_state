fd98e8429b190e1e0ffabc8f78bd2cdd673d60af
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-07-21 20:48:20 -0500

    Mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
+//Stateless vs Stateful
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    print("BUILD!!!!!!");
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              counter.toString(),
+              style: const TextStyle(
+                fontSize: 40.0,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                counter = counter + 10;
+
+                setState(() {});
+
+                print(counter);
+              },
+              child: const Text("Click"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
