4f1e38fa12e585ec48760818fafb3835d695e464
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-07-21 19:49:45 -0500

    He agregado un widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
