8eba67c45845646cafbb2a13289aa87cf0f27bac
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-07-21 21:59:41 -0500

    Mods

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int counter = 100;
+  int counter = 0;
+
+  List<int> numbers = [
+    23, //0
+    11, //1
+    24, //2
+    100, //3
+    56, //4
+    230, //5
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,20 +41,28 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              counter.toString(),
+              numbers[counter].toString(), //0 1 2 3 4 5
               style: const TextStyle(
                 fontSize: 40.0,
               ),
             ),
             ElevatedButton(
               onPressed: () {
-                counter = counter + 10;
-
-                setState(() {});
-
-                print(counter);
+                if (counter < numbers.length - 1) {
+                  counter = counter + 1;
+                  setState(() {});
+                }
               },
-              child: const Text("Click"),
+              child: const Text("Siguiente"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (counter > 0) {
+                  counter = counter - 1;
+                  setState(() {});
+                }
+              },
+              child: const Text("Anterior"),
             ),
           ],
         ),
